069c0329493f24b2d8a026b75ee87705a5fb969d
Honjack <honjack@gmail.com>
2017-09-06 17:13:51 +0800

    work

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,46 @@ Route::post('register',array('before'=>'csrf',function()
         return Redirect::to('register')->withInput()->withErrors($validator);
     }
 }));
+
+Route::get('user/{id}/edit',array('before'=>'auth','as'=>'user.edit',function($id)
+{
+    if(Auth::user()->is_admin or Auth::id()==$id){
+        return view('users.edit')->with('user',User::find($id));
+    }else{
+        return Redirect::to('/');
+    }
+}));
+
+
+Route::put('user/{id}', array('before' => 'auth|csrf', function($id)
+{
+    if (Auth::user()->is_admin or (Auth::id() == $id)) {
+        $user = User::find($id);
+        $rules = array(
+            'password' => 'required_with:old_password|min:4|confirmed',
+            'old_password' => 'min:4',
+        );
+        if (!(Input::get('nickname') == $user->nickname))
+        {
+            $rules['nickname'] = 'required|min:3||unique:users,nickname';
+        }
+        $validator = Validator::make(Input::all(), $rules);
+        if ($validator->passes())
+        {
+            if (!(Input::get('old_password') == '')) {
+                if (!Hash::check(Input::get('old_password'), $user->password)) {
+                    return Redirect::route('user.edit', $id)->with('user', $user)->with('message', array('type' => 'danger', 'content' => 'Old password error'));
+                } else {
+                    $user->password = Hash::make(Input::get('password'));
+                }
+            }
+            $user->nickname = Input::get('nickname');
+            $user->save();
+            return Redirect::route('user.edit', $id)->with('user', $user)->with('message', array('type' => 'success', 'content' => 'Modify successfully'));
+        } else {
+            return Redirect::route('user.edit', $id)->withInput()->with('user', $user)->withErrors($validator);
+        }
+    } else {
+        return Redirect::to('/');
+    }
+}));
